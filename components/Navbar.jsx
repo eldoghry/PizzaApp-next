0d9c826286dfd96d7506ef189f71a896646c0238
mma-90 <moh.mag.ali@gmail.com>
2022-09-08 03:52:08 +0200

    refactor

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,41 +1,37 @@
-import styles from "../styles/Navbar.module.css";
+import classes from "../styles/Navbar.module.css";
 import Image from "next/image";
 import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.item}>
-        <div className={styles.callButton}>
-          <Image
-            src="/img/telephone.png"
-            alt=""
-            width={30}
-            height={30}
-          />
+    <div className={classes.container}>
+      <div className={classes.item}>
+        <div className={classes.callButton}>
+          <Image src="/img/telephone.png" alt="" width={30} height={30} />
         </div>
 
-        <div className={styles.texts}>
-          <span className={styles.text}>order now !</span>
-          <span className={styles.text}>012 345 6789</span>
+        <div className={classes.texts}>
+          <span className={classes.text}>order now !</span>
+          <span className={classes.text}>012 345 6789</span>
         </div>
       </div>
 
-      <div className={styles.item}>
-        <ul className={styles.list}>
-          <li className={styles.listItem}>
+      <div className={classes.item}>
+        <ul className={classes.list}>
+          <li className={classes.listItem}>
             <Link href={`/`}>Home</Link>
           </li>
-          <li className={styles.listItem}>
+          <li className={classes.listItem}>
             <Link href={`#products`}>Products</Link>
           </li>
 
-          <li className={styles.listItem}>
+          <li className={classes.listItem}>
             <Link href={`#products`}>Menu</Link>
           </li>
 
-          <Link href={`/`}>
+          <Link href={`/`} className={classes.NavImgContainer}>
             <Image
+              className={classes.NavImg}
               style={{ cursor: "pointer" }}
               src="/img/logo.png"
               alt="logo"
@@ -44,25 +40,29 @@ const Navbar = () => {
             />
           </Link>
 
-          <li className={styles.listItem}>
+          <li className={classes.listItem}>
             <Link href={`#`}>events</Link>
           </li>
-          <li className={styles.listItem}>
+          <li className={classes.listItem}>
             <Link href={`#`}>content</Link>
           </li>
-          <li className={styles.listItem}>
+          <li className={classes.listItem}>
             <Link href={`#`}>blog</Link>
           </li>
         </ul>
       </div>
 
-      <div className={styles.item}>
+      <div className={classes.item}>
         <Link href={`/cart`}>
-          <div className={styles.cart}>
+          <div className={classes.cart}>
             <Image src="/img/cart.png" alt="" width="35" height="35" />
-            <span className={styles.counter}>3</span>
+            <span className={classes.counter}>3</span>
           </div>
         </Link>
+
+        <div id="toggleMenu" className={classes.toggleMenu}>
+          <span></span>
+        </div>
       </div>
     </div>
   );
